87056edb89ff2f87aa405b2252cb0eacb047bbfd
Akhil-PA <70505102+Akhil-PA@users.noreply.github.com>
2021-12-15 23:59:50 +0530

    Datatype2:program to convert into decimal number system

---
+++ datatype2.py
@@ -0,0 +1,7 @@
+#A program to convert number from octal, binary and hexadecimal system into decimal number system
+a=0o17
+b=0B1110010
+c=0x1c2
+
+print("Octal:0o17 ,Binary: 0B1110010 and Hexadecimal: 0x1c2")
+print("Decimal number: ",a,b,c)
