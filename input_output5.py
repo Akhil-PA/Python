ebf8744701436abe261c7e07b2e02feada3e398c
Akhil-PA <70505102+Akhil-PA@users.noreply.github.com>
2021-12-16 23:52:24 +0530

    Program to accept 3 integers and print it

---
+++ input_output5.py
@@ -0,0 +1,3 @@
+#Program to accept 3 integers in the same line and display their.
+var1,var2,var3=[int(x) for x in input("Enter three numbers:").split()]
+print("Integer 1:",var1,"\nInteger 2:",var2,"\nInteger 3:",var3)
