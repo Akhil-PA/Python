f50e3be2099a4e9a0470e246ca950e5db814cc49
Akhil-PA <70505102+Akhil-PA@users.noreply.github.com>
2021-12-16 16:54:14 +0530

    program to input in other number systems and convert into decimal

---
+++ input_output4.py
@@ -0,0 +1,9 @@
+#Program to  convert numbers from other systems into decimal number system
+str=input("Enter hexadecimal value:")
+print(int(str,16))
+
+str=input("Enter octal value:")
+print(int(str,8))
+
+str=input("Enter binary number:")
+print(int(str,2))
