6e0ae89fe1cc9eda66d0140d77f78e4c45191885
Akhil-PA <70505102+Akhil-PA@users.noreply.github.com>
2021-12-16 07:32:04 +0530

    program to input 2 integer and perform various mathematical operations

---
+++ input_output3.py
@@ -0,0 +1,7 @@
+#Program to input 2 integers and perform various mathematical operation.
+x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
+print("Sum of {} and {}:{}".format(x,y, x+y))
+print("Multiplication of {} and {}:{}".format(x,y, x*y))
+print("Subtraction of {} and {}:{}".format(x,y, x-y))
+print("Division of {} and {}:{}".format(x,y, x/y))
