38733c28b1a9efadf4f69846fe449e874829301b
Akhil-PA <70505102+Akhil-PA@users.noreply.github.com>
2021-12-16 07:10:23 +0530

    program to input different datatype and output it

---
+++ input_output2.py
@@ -0,0 +1,6 @@
+#Program to input different datatypes and output it
+ch=input("Enter a char: ")
+str=input("Enter a string: ")
+x=int(input("Enter a integer number: "))
+y=float(input("Enter a float number: "))
+print("Character:",ch,"\nString:",str,"\nInterger:",x,"\nFloat number:",y)
